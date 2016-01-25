7e025b8cd6f16b44a1d1ebdbf13ed8a521d3b201
Roger Steve Ruiz <hi@rog.gr>
2016-01-25 11:45:46 -0500

    Add instructions for rebuilding assets

--- assets/start.js
+++ assets/start.js
@@ -1,3 +1,4 @@
 console.log( 'Welcome to the npm front-end starter' );
 console.log( 'edit assets/start.js' );
 console.log( 'and include any require statements as needed' );
+console.log( 'then run `npm run build` at the directory root to rebuild the project' );
